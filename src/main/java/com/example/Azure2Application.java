d32421d38ffe7cd059296e210266111de8809c8f
partha <partha.bhakat@gmail.com>
2019-12-17 16:41:50 +0530

    commit to local repo

---
+++ src/main/java/com/example/Azure2Application.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Azure2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Azure2Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/Azure2Controller.java
@@ -0,0 +1,18 @@
+package com.example;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Azure2Controller {
+
+	@GetMapping("/azure2")
+	public String azure2method() {
+		return "from azure 2 project";
+	}
+	
+	@GetMapping("/home")
+	public String azure2home() {
+		return "same endpoint as azure1 project";
+	}
+}
