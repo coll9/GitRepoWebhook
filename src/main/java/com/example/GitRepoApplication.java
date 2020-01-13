464ba3982cefbaf0b3e0b77241242e4ba2b9a041
partha <partha.bhakat@gmail.com>
2020-01-13 21:06:38 +0530

    changed class names

--- src/main/java/com/example/GitRepoApplication.java
+++ src/main/java/com/example/GitRepoApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Azure2Application {
+public class GitRepoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Azure2Application.class, args);
+		SpringApplication.run(GitRepoApplication.class, args);
 	}
 
 }
--- src/test/java/com/example/GitRepoApplicationTests.java
+++ src/test/java/com/example/GitRepoApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Azure2ApplicationTests {
+class GitRepoApplicationTests {
 
 //	@Test
 //	void contextLoads() {
